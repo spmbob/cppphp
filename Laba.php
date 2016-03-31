a05f536668509c316c2cb265d80443a3c4e4c084
spmbob <urban_manki@mail.ru>
2016-03-31 09:31:41 +0300

    Labphp

---
+++ Laba.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+<?php
+$arrayMod=arrayMod(300);   
+for ($count=0;$count<=300;$count++){
+	
+$array[count]= count+1; 
+}
+
+$divider = 0; 
+$ArrayDivider= array(300); 
+$ad = 0;
+ 
+for ($i = 0;$i<=300;$i++)
+{
+if (($arrayMod[$i]%arrayMod[j])==0)
+{
+if ((arrayMod[ad] = ArrayMod[j];
+}
+};
+if ($divider==9)
+{
+	
+$sum+=$arrayMod[$i];
+echo "<br />";
+echo$arrayMod[$i];
+echo",";
+}
+echo"<br />";
+echo$sum;
+
+
+?>
